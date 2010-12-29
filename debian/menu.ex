906a0e3af561012ac32a9bef6494f83551303b95
Jeremy Salwen <jeremybubs@gmail.com>
2010-12-29 01:48:06 -0500

    added debian structure

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(so-666):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="so-666" command="/usr/bin/so-666"
---
+++ debian/so-666.default.ex
@@ -0,0 +1,10 @@
+# Defaults for so-666 initscript
+# sourced by /etc/init.d/so-666
+# installed at /etc/default/so-666 by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
