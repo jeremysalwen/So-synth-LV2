955202972ec0d8f7147911e382e9fcb60b1a8bc6
Jeremy Salwen <jeremybubs@gmail.com>
2010-12-29 02:04:09 -0500

    Fixed up debian packaging

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(so-666):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="so-666" command="/usr/bin/so-666"
--- debian/so-666.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the so-666 package
-#
-0 4	* * *	root	[ -x /usr/bin/so-666_maintenance ] && /usr/bin/so-666_maintenance
